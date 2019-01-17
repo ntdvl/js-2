7ac48cff6e8cc42b2711e967f415cb44b8a20691
ntdvl <yilmazkoman@outlook.com>
2019-01-17 20:52:40 +0300

    Added 8. Gun

---
+++ 8. Gun/1. Calisma/app.js
@@ -0,0 +1,5 @@
+var x = 'Javascript, HTML5, CSS3';
+
+var y = x.slice(2,11);
+
+console.log(y);
---
+++ 8. Gun/2. Calisma/app.js
@@ -0,0 +1,19 @@
+var x = 'A theme that has been built with the latest WordPress technology';
+
+console.log(x);
+
+var y = x.substring(5, 15);
+
+console.log('substring(5, 15) : ' +  y);
+
+
+console.log('\n\n');
+
+var z = 'A theme that has been built with the latest WordPress technology';
+
+console.log(z);
+
+
+var q = z.substr(5, 15);
+
+console.log('substr(5, 15) : ' +  q);
