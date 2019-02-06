5782829da660226fc76509f3390623df4fdebcb7
ntdvl <yilmazkoman@outlook.com>
2019-02-06 20:51:47 +0300

    Added 14. Gun

---
+++ 14. Gun/1. Calisma/app.js
@@ -0,0 +1,4 @@
+var x = 'ab';
+var y = 'ab';
+
+console.log(x.localeCompare(y));
---
+++ 14. Gun/2. Calisma/app.js
@@ -0,0 +1,5 @@
+var strr = 'Shinji KagawA';
+
+console.log('strr.charAt(5) : ' + strr.charAt(5));
+console.log(strr.charCodeAt(12));
+console.log(String.fromCharCode(65, 123, 125, 126));
