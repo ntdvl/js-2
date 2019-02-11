87bb1be7064dd3d3fb62697a0aaaa01cf565bbbb
ntdvl <yilmazkoman@outlook.com>
2019-02-11 21:29:54 +0300

    Added 17. Gun

---
+++ 17. Gun/1. Calisma/app.js
@@ -0,0 +1,7 @@
+var div = document.getElementsByTagName('div')[0].textContent
+
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+
+    document.getElementsByTagName('div')[0].textContent = 'test';
+
+});
---
+++ 17. Gun/2. Calisma/app.js
@@ -0,0 +1,7 @@
+var str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ducimus recusandae enim sint corrupti repellendus hic, neque reiciendis quis. Minus doloribus sunt numquam quam soluta consectetur ad cumque ut deleniti.';
+
+document.write(str + '<br />');
+
+var result = str.fontsize(5).fontcolor('red');
+
+document.write(result);
