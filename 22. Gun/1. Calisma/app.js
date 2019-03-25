727e4fb551ec40978b8786a052268c170af9020b
ntdvl <yilmazkoman@outlook.com>
2019-03-25 21:58:51 +0300

    Added 22. Gun

---
+++ 22. Gun/1. Calisma/app.js
@@ -0,0 +1,10 @@
+var x = 10;
+var y = 15;
+
+function test() {
+
+    return x + y;
+
+}
+
+document.write(test());
---
+++ 22. Gun/2. Calisma/app.js
@@ -0,0 +1,8 @@
+function test() {
+
+    var x = 19;
+    var y = 26;
+
+}
+
+document.write(Window.x);
