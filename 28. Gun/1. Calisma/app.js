879d3e6d13cc8c482fd24c7e9e9732cbfc770ae7
ntdvl <yilmazkoman@outlook.com>
2019-04-17 21:53:02 +0300

    Added 28. Gun

---
+++ 28. Gun/1. Calisma/app.js
@@ -0,0 +1,12 @@
+const vary = new Array(7);
+
+vary[0] = 'Sunday';
+vary[1] = 'Monday';
+vary[2] = 'Tuesday';
+vary[3] = 'Wednesday';
+vary[4] = 'Thursday';
+vary[5] = 'Friday';
+vary[6] = 'Saturday';
+vary[7] = 'Saturay';
+
+console.log(vary.length);
---
+++ 28. Gun/2. Calisma/app.js
@@ -0,0 +1,6 @@
+var port = new Array(6);
+
+port['sıfır'] = 'HTML5';
+port['bir'] = 'CSS3';
+
+document.write(port['sıfır']);
